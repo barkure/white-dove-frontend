e5ab5805c0fb9b2864dd7346d6dabc8c1bbbab4e
barkure <43804451+barkure@users.noreply.github.com>
2023-11-11 15:46:18 +0800

    设置环境变量，以便生成Docker镜像

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,10 @@
 // config.js
 
+// 从环境变量中获取后端API的基本URL
 const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+// 如果要自己自己部署前后端，则应注释掉上面一行，然后取消下面一行的注释，并按需修改
+// const backendUrl = http://localhost:8000;
+
 // 从环境变量中获取后端API的基本URL
 const config = {
   Backend_baseURL: backendUrl,
