42d687bd153150af2d6e56394ad617c32d4776c2
barkure <43804451+barkure@users.noreply.github.com>
2023-11-11 15:11:34 +0800

    设置Docker环境变量

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,9 @@
 // config.js
+
+const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+// 从环境变量中获取后端API的基本URL
 const config = {
-  Backend_baseURL: "http://localhost:8000",
+  Backend_baseURL: backendUrl,
   // 后端API的基本URL
 };
 
